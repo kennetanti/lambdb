81d4fc703d00da72360c8d1ea52870b728d37acb
Gitea <gitea@fake.local>
2018-11-09 07:45:58 -0700

    basic sql parser code found, now i must make it do all the things not just select

---
+++ TODO.md
@@ -0,0 +1,8 @@
+# to be anything
+
+1. actually write an sql parser for python that can parse stuff besides SELECT
+2. make that do the things the sql says to do
+3. write db backends and clickhouse emution thing?
+4. then just use it to convert all the things to rethinkdb
+
+
---
+++ clickhouseEmulator.py
@@ -0,0 +1,22 @@
+from flask import Flask,redirect,render_template,request,jsonify
+
+from queryTypes import SQLQuery
+
+app = Flask(__name__)
+
+@app.route('/', methods=["GET", "POST"])
+def rootQuery():
+	if request.method == "POST": # clickhouse POST body query
+		if request.args.get('query', False):
+			query = str(request.args['query'])+" "+request.data
+		else:
+			query = request.data
+		result = SQLQuery(query)
+	elif request.method == "GET": # this is clickhouse GET query
+		if request.args.get('query', False):
+			result = SQLQuery(request.args['query'])
+		else: # just plain old / returns Ok
+			return 'Ok'
+
+if __name__ == '__main__':
+    app.run()
