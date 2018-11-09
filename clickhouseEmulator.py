8407f9d521189e475bba5a6ba6d898da0b2a64c7
Gitea <gitea@fake.local>
2018-11-09 07:55:04 -0700

    something

--- clickhouseEmulator.py
+++ clickhouseEmulator.py
@@ -1,22 +1,24 @@
-from flask import Flask,redirect,render_template,request,jsonify
+from flask import Flask, redirect, render_template, request, jsonify
 
 from queryTypes import SQLQuery
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=["GET", "POST"])
 def rootQuery():
-	if request.method == "POST": # clickhouse POST body query
+	if request.method == "POST":  # clickhouse POST body query
 		if request.args.get('query', False):
-			query = str(request.args['query'])+" "+request.data
+			query = str(request.args['query']) + " " + request.data
 		else:
 			query = request.data
 		result = SQLQuery(query)
-	elif request.method == "GET": # this is clickhouse GET query
+	elif request.method == "GET":  # this is clickhouse GET query
 		if request.args.get('query', False):
 			result = SQLQuery(request.args['query'])
-		else: # just plain old / returns Ok
+		else:  # just plain old / returns Ok
 			return 'Ok'
 
+
 if __name__ == '__main__':
-    app.run()
+	app.run()
--- queryTypes.py
+++ queryTypes.py
@@ -1,30 +1,36 @@
 from sqlparse import parse
 from enum import Enum
 
+
 class CRUD(Enum):
 	create = 1
 	read = 2
 	update = 3
 	delete = 4
-class SortStyle(Enum)
+
+
+class SortStyle(Enum):
 	noSort = 0
 	ascending = 1
 	descending = 2
 
+
 class Query:
 	crudType = CRUD.create
-	what = None # select WHAT
-	db = None # from DB.TABLENAME
+	what = None  # select WHAT
+	db = None  # from DB.TABLENAME
 	tablename = None
-	filter = None # where FILTER
+	filter = None  # where FILTER
 	sorting = SortStyle.noSort
-	
-	values = {} # for insert and update	
-	then = None # for multi queries
+
+	values = {}  # for insert and update	
+	then = None  # for multi queries
+
 
 class NotYet(NotImplemented):
 	def __str__(self):
 		return "this shit aint implemented yet"
+
 
 class SQLQuery(Query):
 	def __init__(self, sql=""):
@@ -33,9 +39,8 @@ class SQLQuery(Query):
 		for statement in thing:
 			keyword = statement[0].normalized
 			if keyword not in ["SELECT", "INSERT", "UPDATE", "DELETE"]:
-				print "got unsupported query: "+statement.normalized)
-				continue # for now
+				print "got unsupported query: " + statement.normalized
+				continue  # for now
 			for kw in statement:
 				if kw.is_whitespace:
 					continue
-				
